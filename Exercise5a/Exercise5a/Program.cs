02dcaef563b57bcbba6d65b0e4b28407487d4203
Nguyễn Thành Nhân <nhocksok1009@gmail.com>
2019-10-08 17:35:58 +0700

    Exercise5

--- Exercise5a/Exercise5a/Program.cs
+++ Exercise5a/Exercise5a/Program.cs
@@ -10,6 +10,87 @@ namespace Exercise5a
     {
         static void Main(string[] args)
         {
+            Print_a_2_z_v1();
+            Print2();
+            Print_a_2_z_v2();
+            Print_a_2_z_v3();
+            Print_a_2_z_v4();
+           while(true)
+            {
+                char c = Console.ReadKey().KeyChar;
+                Console.WriteLine(" " + Char2Code(c));
+            }
+
+        }
+        public static void Print_a_2_z_v1()
+        {
+            for (int i = 97; i < 123; i++)
+            {
+                Console.Write((char)i);
+            }     
+        }
+        public static void Print_a_2_z_v2()
+        {
+            char c = ' ';
+            for(int i =97;i<=122;i++)
+            {
+                c = (char)i;
+                Console.Write(c);
+            }
+        }
+        public static void Print_a_2_z_v3()
+        {
+            char c = 'a';
+            for (int i = 0; i <26; i++)
+            {
+                Console.WriteLine(c);
+                c++;
+            }
+        }
+        public static void Print_a_2_z_v4()
+        {          
+            for (char c = 'a'; c<= 'z'; c++)
+            {
+                Console.WriteLine(c);                
+            }
+        }
+        public static void Print2()
+        {
+            for (int i = 122; i > 96; i--)
+            {
+                Console.Write((char)i);
+            }
+            //3//
+            for (int i = 65; i < 91; i++)
+            {
+                Console.Write((char)i);
+            }
+            //4//
+            for (int i = 0; i < 256; i++)
+            {
+                Console.WriteLine((char)i);
+                Console.WriteLine(i);
+            }
+        }
+        public static int Char2Code(char c)
+        {
+            return (int)c;
+        }
+        public static bool IsUpper(char c)
+        {
+            return false;
+        }
+        public static bool IsLower(char c)
+        {
+            return false;
+        }
+        public static char ToUpper(char c)
+        {
+            return c;
+        }
+        public static char ToLower(char c)
+        {
+            return c;
         }
     }
 }
